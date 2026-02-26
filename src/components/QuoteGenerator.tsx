c6467f26bd0702b5ebac009fb7f2ab3515eb525d
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2026-02-26 19:32:57 +0530

    Added New Quote Generate with Button and Loader

--- src/components/QuoteGenerator.tsx
+++ src/components/QuoteGenerator.tsx
@@ -1,32 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
+import './QuoteGenerator.css';
 
 const QuoteGenerator = () => {
 
-  const [quote, setQuote] = useState("")
+  const [quote, setQuote] = useState({ content: "", author: "" });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchQuote();
+  }, []);
 
-  }, [])
-  
-  const fetchQuote = async() => {
+  const fetchQuote = async () => {
     try {
       const response = await fetch("https://dummyjson.com/quotes/random");
       const data = await response.json();
-      console.log(data);
-      setQuote(data.quote)
+      console.log(data)
+      setQuote({
+        content: data.quote,
+        author: data.author
+      });
     } catch (error) {
-      
-    }
+      console.log(error);
+    } finally {
+      setLoading(false);
+   }
   }
 
   return (
-    <div className="quote-generator">
-      <h1>Inspiration</h1>
+    <div className='quote-generator'>
 
-      <p>{quote}</p>
+      <div className='quote-container'>
+        
+        <h1>Daily Inspiration</h1>
+
+        {loading &&
+          <div className='loading-content'>
+           <div className='loading-spinner'></div>
+           <div className='loading-text'>Loading an awesome Quote...</div>
+          </div>
+        }
+
+        {!loading && quote.content && (
+          <div className='quote-content'>
+            <div className="quote-text">{quote.content}</div>
+            <div className="quote-author">{quote.author}</div>
+          </div>
+        )}
+
+        <button className='generate-btn' onClick={fetchQuote} disabled={loading}>
+          {loading ? "Loading..." : "Generate New Qoute"}
+        </button>
+
+      </div>
     </div>
   )
 }
 
-export default QuoteGenerator
+export default QuoteGenerator;
