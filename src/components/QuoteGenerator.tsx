6bcd741d1a8f88964fd403a24236d32aad982350
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2026-02-26 19:19:25 +0530

    Generate Quote using fetch and useState

--- src/components/QuoteGenerator.tsx
+++ src/components/QuoteGenerator.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 
 const QuoteGenerator = () => {
+
+  const [quote, setQuote] = useState("")
+
+  useEffect(() => {
+    fetchQuote();
+
+  }, [])
+  
+  const fetchQuote = async() => {
+    try {
+      const response = await fetch("https://dummyjson.com/quotes/random");
+      const data = await response.json();
+      console.log(data);
+      setQuote(data.quote)
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <div>QuoteGenerator</div>
+    <div className="quote-generator">
+      <h1>Inspiration</h1>
+
+      <p>{quote}</p>
+    </div>
   )
 }
 
